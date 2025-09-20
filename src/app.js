d689a97ced4a120f0c8ebfd4afbe5d60498ece89
krishtian90 <krishtian90@gmail.com>
2025-09-20 02:52:39 +0000

    nombres-dominio-krishtian-extras

--- src/app.js
+++ src/app.js
@@ -20,4 +20,27 @@ generadordominios()
 
 
 
+let pronoun2 = ['the', 'our'];
+let adj2 = ['great', 'big'];
+let noun2 = ['inter', 'rabi'];
+let ext2 = ['.com', '.net','.com','.net'];
+
+
+function generadordominios2() {
+  for (let e = 0; e < pronoun.length; e++){
+    for (let f = 0; f < adj.length; f++){
+      for (let g = 0; g < noun.length; g++){
+        for (let h = 0; h < ext.length; h++){
+
+          console.log(`${pronoun2[e]} ${adj2[f]} ${noun2[g]} ${ext2[h]}`)
+        } 
+      }
+    }
+  }
+}        
+generadordominios2()
+
+
+
+
 
